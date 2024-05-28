5071da2cdab2c60a0735aacb65b39167cf947593
Oleksii <shcherbaalex77@gmail.com>
2024-05-28 11:17:43 -0500

    added base structure

---
+++ src/components/ContactList/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import propsTypes from "prop-types";
+import css from "./Contact.module.css";
+import { useDispatch } from "react-redux";
+import { deleteContact } from "../../../redux/contactsOps";
+
+const Contact = ({ id, name, phone }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <li className={css.contactElement}>
+      <ul className={css.dataList}>
+        <li className={css.name}>{name}</li>
+        <li className={css.phone}>{phone}</li>
+      </ul>
+      <button
+        className={css.deleteAction}
+        onClick={() => dispatch(deleteContact(id))}
+      >
+        Delete
+      </button>
+    </li>
+  );
+};
+
+Contact.propsTypes = {
+  id: propsTypes.string.isRequired,
+  name: propsTypes.string.isRequired,
+  phone: propsTypes.string.isRequired,
+};
+
+export default Contact;
