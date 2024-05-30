6a4d16fb1774a6dab29980aaf9203713ad48a045
Oleksii <shcherbaalex77@gmail.com>
2024-05-30 05:01:24 -0500

    added routes system

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,56 @@
 import "./App.css";
 import "modern-normalize";
 
-import ContactForm from "./components/ContactForm/ContactForm";
-import SearchBox from "./components/SearchBox/SearchBox";
-import ContactList from "./components/ContactList/ContactList";
-
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { fetchContacts } from "./redux/contacts/operations";
+import { useEffect, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Layout from "./components/Layout";
+import PrivateRoute from "./components/PrivateRoute";
+import RestrictedRoute from "./components/RestrictedRoute";
+
+import { refreshUser } from "./redux/auth/operations";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const ContactsPage = lazy(() => import("./pages/ContactsPage/ContactsPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
+const RegistrationPage = lazy(() =>
+  import("./pages/RegistrationPage/RegistrationPage")
+);
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshUser());
   }, [dispatch]);
 
   return (
     <div className="mainBlock">
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/contacts"
+            element={<PrivateRoute component={ContactsPage} />}
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute component={LoginPage} redirectTo="/contacts" />
+            }
+          />
+          <Route
+            path="/registration"
+            element={
+              <RestrictedRoute
+                component={RegistrationPage}
+                redirectTo="/contacts"
+              />
+            }
+          />
+        </Routes>
+      </Layout>
     </div>
   );
 }
---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { selectIsLoggedIn } from "../redux/auth/selectors";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ component, redirectTo = "/" }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  return isLoggedIn ? component : <Navigate to={redirectTo} />;
+};
+
+export default PrivateRoute;
